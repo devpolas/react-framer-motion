b55c4b1f3c626eb4735aa1d78f7639b6ad624288
Polas Chandra Barmon <195308698+devpolas@users.noreply.github.com>
2025-10-26 18:14:02 +0600

    icon animation with framer motion

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,14 +1,14 @@
-import { createPortal } from 'react-dom';
+import { createPortal } from "react-dom";
 
 export default function Modal({ title, children, onClose }) {
   return createPortal(
     <>
-      <div className="backdrop" onClick={onClose} />
-      <dialog open className="modal">
+      <div className='backdrop' onClick={onClose} />
+      <dialog open className='modal'>
         <h2>{title}</h2>
         {children}
       </dialog>
     </>,
-    document.getElementById('modal')
+    document.getElementById("modal")
   );
 }
